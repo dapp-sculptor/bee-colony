35b99bcd11f82576c686cd640ce7019495c629ae
dapp-sculptor <hugodevpro@outlook.com>
2024-02-28 06:55:19 +0900

    feat: push

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,12 +1,14 @@
 import { CustomBtn, Hexagons, Bubbles } from "@/components";
 import { Pattern, HexaPattern } from "@/assets/images"
 import { useEffect } from "react";
+import { useApp } from "@/context";
 
 const Landing = () => {
   const handleMint = () => { }
 
   const handleCollection = () => { }
 
+  const {landscape} = useApp()
   useEffect(() => {
     // @ts-ignore
     const bg = HexaPattern
@@ -14,16 +16,15 @@ const Landing = () => {
 
   return <>
     <div className={`w-[100vw] min-[900px]:h-[calc(90vh)] border-t-[1px] flex flex-row bg-[linear-gradient(to_bottom,#301764,#4610be)] relative overflow-hidden px-[10vw]`}>
-      {/* <div className={`opacity-20 bg-[url('./src/assets/hexapattern.png')] h-[100%] top-0 absolute z-60} `} style={{backgroundImage:'/src/assets/hexapattern.png',background}}></div> */}
-      <div className={`opacity-20 h-[100%] top-0 absolute z-60} `} style={{backgroundImage:`url(${HexaPattern})`}}></div>
+      <div className={`h-[100%] top-0 absolute z-60} bg-[url('../../assets/images/hexapattern.png)]`} ></div>
       <img src={Pattern} className="absolute bottom-0 w-[100vw] z-20" />
-      <div className="flex max-[900px]:flex-col w-[100%]">
+      <div className={`flex ${landscape}:flex-col w-[100%]`}>
         <div className="min-[900px]:w-[60%] text-[white] z-50 flex flex-col items-center justify-center py-[90px]">
           <div>
             <div className="text-[4vh]">Discover</div>
             <div className="text-[6vh] pt-[5vh] font-fenomen-sans-book ">Collect and Sell</div>
             <div className="text-[7vh] pb-[5vh] font-nippori">Hype Bees</div>
-            <div className="text-[3vh] w-[40vw] break-words">Lorem ipsum dolor sit amet consectetuer.<br/> Semper vel fermentum in lacus sem ut sed</div>
+            <div className="text-[3vh] w-[40vw] break-words">Lorem ipsum dolor sit amet consectetuer.<br /> Semper vel fermentum in lacus sem ut sed</div>
             <div className="flex gap-[1vw] mt-[5vh]">
               <CustomBtn name='Mint' feature={handleMint} />
               <CustomBtn name='Collection' feature={handleCollection} />
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -2,29 +2,41 @@ import { Link, Outlet } from "react-router-dom";
 
 import { NavBtn, WalletConnect } from "@/components";
 import { navButtonList } from "@/data/constant";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Hamburger } from "@/assets/images";
+import { useApp } from "@/context";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 const Layout = () => {
   const [showModal, setShowModal] = useState<boolean>(false)
+  const { landscape, setLandscape, } = useApp()
+  const size = useWindowSize();
+
+  useEffect(() => {
+    const W = size.width
+    const H = size.height
+    console.log("====>", size, Number(H) > Number(W) * 0.64)
+    setLandscape(Number(H) > Number(W) * 0.64)
+  }, [size])
+
   return (
     <>
       <div className="h-[10vh] w-[100vw] bg-[#201c2c] flex items-center justify-between px-[5vh]">
         <Link to='/' className="uppercase text-[white] text-[5vh] cursor-pointer hover:scale-110 hover:text-[white] font-nippori" >the hive</Link>
-        {/* <div className="flex gap-[5px] min-[900px]:gap-[20px] items-center"> */}
         <div className="flex gap-[1vh]  items-center">
-          <div className="hidden min-[900px]:flex gap-[3vh]">
+          <div className={`${landscape ? "" : "hidden"}  min-[900px]:flex gap-[3vh]`}>
             {navButtonList.map((
               value, index) => {
               return <NavBtn key={index} name={value.name} link={value.link} />
             })
             }
           </div>
-          <div className="min-[900px]:hidden">
-            <div className={`w-[30px] h-[30px] flex justify-center items-center`} onClick={() => setShowModal(true)}>
-              <img src={Hamburger}/>
+          {/* <div className={``} style={{ display: `${landscape?'none':'block'}` }}> */}
+          <div  className={`${!landscape ? "flex" : "hidden"}`}>
+            <div className={`flex justify-center items-center`} onClick={() => setShowModal(true)} >
+              <img src={Hamburger} className={`w-[5vh] h-[5vh] ${landscape ? "hidden" : ""}`}/>
             </div>
-            <div className={`absolute flex justify-center items-center ${showModal ? "" : "hidden"} w-[100vw] h-[100vh] left-0 top-0 `}>
+            <div className={`absolute flex justify-center items-center ${showModal ? "" : "hidden"}  left-0 top-0 `}>
               <div className="absolute backdrop-blur-[5px] z-[60] w-[100vw] h-[1239px] " onClick={() => setShowModal(false)} ></div>
               <div className="fixed flex flex-col gap-[40px] text-[30] z-[70]">
                 {navButtonList.map((
